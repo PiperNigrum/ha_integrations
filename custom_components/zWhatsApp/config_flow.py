ae520d62a000a41f49dfd862c89f425a88eb87c0
Tobias Kammerl-Hölzlwimmer <tobias.kammerl@web.de>
2026-02-21 22:42:15 +0100

    Update config_flow.py with new WhatsAppConfigFlow class implementation

--- custom_components/zWhatsApp/config_flow.py
+++ custom_components/zWhatsApp/config_flow.py
@@ -7,23 +7,22 @@ from .const import DOMAIN, CONF_BASE_URL, CONF_PORT
 class WhatsAppConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
-        async def async_step_user(self, user_input=None):
-                errors = {}
+    async def async_step_user(self, user_input=None):
+        errors = {}
 
-                        if user_input is not None:
-                                    return self.async_create_entry(
-                                                    title="WhatsApp Client",
-                                                                    data=user_input
-                                                                                )
+        if user_input is not None:
+            return self.async_create_entry(
+                title="WhatsApp Client",
+                data=user_input
+            )
 
-                                                                                        schema = vol.Schema({
-                                                                                                    vol.Required(CONF_BASE_URL): str,
-                                                                                                                vol.Required(CONF_PORT): int
-                                                                                                                        })
+        schema = vol.Schema({
+            vol.Required(CONF_BASE_URL): str,
+            vol.Required(CONF_PORT): int
+        })
 
-                                                                                                                                return self.async_show_form(
-                                                                                                                                            step_id="user",
-                                                                                                                                                        data_schema=schema,
-                                                                                                                                                                    errors=errors
-                                                                                                                                                                            )
-                                                                                                                                                                            
+        return self.async_show_form(
+            step_id="user",
+            data_schema=schema,
+            errors=errors
+        )
