decf377e5c5ace30218d0cc00b5cccc8f8282160
Tobias Kammerl-Hölzlwimmer <tobias.kammerl@web.de>
2026-02-22 11:46:10 +0100

    notify.py aktualisieren

--- custom_components/zWhatsApp/notify.py
+++ custom_components/zWhatsApp/notify.py
@@ -1,31 +1,33 @@
 from homeassistant.components.notify import BaseNotificationService
 
 async def async_get_service(hass, config, discovery_info=None):
-    # Name des Notify-Dienstes festlegen
-    return WhatsAppNotificationService(hass, "whatsapp_send_message")
+    # discovery_info entscheidet, welcher Dienst erzeugt wird
+    service_type = discovery_info.get("service")
+
+    if service_type == "send_message":
+        return WhatsAppSendMessageService(hass, "whatsapp_send_message")
+
+    if service_type == "send_media":
+        return WhatsAppSendMediaService(hass, "whatsapp_send_media")
+
+    return None
 
 
-class WhatsAppNotificationService(BaseNotificationService):
+class WhatsAppSendMessageService(BaseNotificationService):
     def __init__(self, hass, name):
         self.hass = hass
         self._name = name
 
     @property
     def name(self):
-        # Name des Notify-Dienstes → notify.whatsapp_send_message
         return self._name
 
     async def async_send_message(self, message="", **kwargs):
         title = kwargs.get("title", "")
-
-        # Kombinierter WhatsApp-Text
         combined_message = f"*{title}*\n{message}" if title else message
 
-        # chat_id aus Sensor lesen
-        chat_state = self.hass.states.get("sensor.whatsapp_tobias")
-        chat_id = chat_state.state if chat_state else ""
+        chat_id = self.hass.states.get("sensor.whatsapp_tobias").state
 
-        # WhatsApp.send_Media aufrufen
         await self.hass.services.async_call(
             "WhatsApp",
             "send_message",
@@ -33,5 +35,43 @@ class WhatsAppNotificationService(BaseNotificationService):
                 "chat_id": chat_id,
                 "message": combined_message
             },
+            blocking=True
+        )
+
+
+class WhatsAppSendMediaService(BaseNotificationService):
+    def __init__(self, hass, name):
+        self.hass = hass
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    async def async_send_message(self, message="", **kwargs):
+        data = kwargs.get("data", {})
+        title = kwargs.get("title", "")
+
+        caption = f"*{title}*\n{message}" if title else message
+
+        chat_id = self.hass.states.get("sensor.whatsapp_tobias").state
+
+        payload = {
+            "chat_id": chat_id,
+            "url": data.get("url"),
+            "caption": caption,
+            "sendMediaAsDocument": data.get("sendMediaAsDocument", False),
+            "sendAudioAsVoice": data.get("sendAudioAsVoice", False),
+            "sendVideoAsGif": data.get("sendVideoAsGif", False),
+            "sendMediaAsSticker": data.get("sendMediaAsSticker", False),
+            "sendMediaAsHd": data.get("sendMediaAsHd", False),
+            "isViewOnce": data.get("isViewOnce", False),
+            "linkPreview": data.get("linkPreview", False),
+        }
+
+        await self.hass.services.async_call(
+            "WhatsApp",
+            "send_media",
+            payload,
             blocking=True
         )
