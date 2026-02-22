fd78768ace60c87104c321dc7bf59baec8bc4d28
Tobias Kammerl-Hölzlwimmer <tobias.kammerl@web.de>
2026-02-22 13:58:27 +0100

    __init__.py aktualisieren

--- custom_components/zWhatsApp/__init__.py
+++ custom_components/zWhatsApp/__init__.py
@@ -3,20 +3,23 @@ from homeassistant.config_entries import ConfigEntry
 
 from .const import DOMAIN
 
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up WhatsApp integration from a config entry."""
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
-    # Notify-Plattform laden
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "notify")
-    )
+    # Neue HA API: forward_entry_setups erwartet eine LISTE
+    await hass.config_entries.async_forward_entry_setups(entry, ["notify"])
 
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "notify")
+    """Unload WhatsApp integration."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["notify"])
+
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
+
     return unload_ok
