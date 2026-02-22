324a0a9428ab5cd8a3bd7bb2b0a0d797c2e03959
Tobias Kammerl-Hölzlwimmer <tobias.kammerl@web.de>
2026-02-22 11:37:20 +0100

    Update config_flow.py with WhatsAppConfigFlow class implementation.

---
+++ config_flow.py
@@ -0,0 +1,38 @@
+import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.core import callback
+
+from .const import DOMAIN, CONF_BASE_URL, CONF_PORT
+
+class WhatsAppConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        errors = {}  
+
+        if user_input is not None:
+            # Check if an existing entry exists
+            existing_entry = await self.async_set_unique_id(DOMAIN)
+            if existing_entry:
+                self.hass.config_entries.async_update_entry(
+                    existing_entry,
+                    data=user_input
+                )
+                self._abort_if_unique_id_configured()
+            else:
+                # Create a new entry
+                return self.async_create_entry(
+                    title="WhatsApp Client",
+                    data=user_input
+                )
+
+        schema = vol.Schema({
+            vol.Required(CONF_BASE_URL): str,
+            vol.Required(CONF_PORT): int
+        })
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=schema,
+            errors=errors
+        )
