c88d376cf2f02e2c3efdd79bbbc22cec9167afdf
Tobias Kammerl-Hölzlwimmer <tobias.kammerl@web.de>
2026-02-23 11:13:41 +0100

    notify.py aktualisieren

--- custom_components/whatsapp/notify.py
+++ custom_components/whatsapp/notify.py
@@ -2,56 +2,76 @@ from homeassistant.components.notify import BaseNotificationService
 
 
 async def async_get_service(hass, config, discovery_info=None):
-    return WhatsAppNotifyService(hass)
+    # Zwei getrennte Notify-Services zurückgeben
+    return [
+        WhatsAppSendMessageNotifyService(hass, "whatsapp_send_message"),
+        WhatsAppSendMediaNotifyService(hass, "whatsapp_send_media"),
+    ]
 
 
-class WhatsAppNotifyService(BaseNotificationService):
-    def __init__(self, hass):
+class WhatsAppSendMessageNotifyService(BaseNotificationService):
+    def __init__(self, hass, name):
         self.hass = hass
+        self._name = name
 
     @property
-    def targets(self):
-        return {
-            "whatsapp_send_message": "whatsapp_send_message",
-            "whatsapp_send_media": "whatsapp_send_media",
-        }
+    def name(self):
+        # ergibt notify.whatsapp_send_message
+        return self._name
 
     async def async_send_message(self, message="", **kwargs):
-        target = kwargs.get("target")
         title = kwargs.get("title", "")
-        data = kwargs.get("data", {})
+        combined = f"*{title}*\n{message}" if title else message
 
-        chat_id = self.hass.states.get("sensor.whatsapp_tobias").state
+        chat_id_state = self.hass.states.get("sensor.whatsapp_tobias")
+        chat_id = chat_id_state.state if chat_id_state else ""
 
-        if target == "whatsapp_send_media":
-            payload = {
+        await self.hass.services.async_call(
+            "whatsapp",
+            "send_message",
+            {
                 "chat_id": chat_id,
-                "url": data.get("url"),
-                "caption": f"*{title}*\n{message}" if title else message,
-                "sendMediaAsDocument": data.get("sendMediaAsDocument", False),
-                "sendAudioAsVoice": data.get("sendAudioAsVoice", False),
-                "sendVideoAsGif": data.get("sendVideoAsGif", False),
-                "sendMediaAsSticker": data.get("sendMediaAsSticker", False),
-                "sendMediaAsHd": data.get("sendMediaAsHd", False),
-                "isViewOnce": data.get("isViewOnce", False),
-                "linkPreview": data.get("linkPreview", False),
-            }
+                "message": combined,
+            },
+            blocking=True,
+        )
 
-            await self.hass.services.async_call(
-                "whatsapp",
-                "send_media",
-                payload,
-                blocking=True,
-            )
-        else:
-            combined = f"*{title}*\n{message}" if title else message
 
-            await self.hass.services.async_call(
-                "whatsapp",
-                "send_message",
-                {
-                    "chat_id": chat_id,
-                    "message": combined,
-                },
-                blocking=True,
-            )
+class WhatsAppSendMediaNotifyService(BaseNotificationService):
+    def __init__(self, hass, name):
+        self.hass = hass
+        self._name = name
+
+    @property
+    def name(self):
+        # ergibt notify.whatsapp_send_media
+        return self._name
+
+    async def async_send_message(self, message="", **kwargs):
+        title = kwargs.get("title", "")
+        data = kwargs.get("data", {}) or {}
+
+        caption = f"*{title}*\n{message}" if title else message
+
+        chat_id_state = self.hass.states.get("sensor.whatsapp_tobias")
+        chat_id = chat_id_state.state if chat_id_state else ""
+
+        payload = {
+            "chat_id": chat_id,
+            "url": data.get("url"),
+            "caption": caption,
+            "sendMediaAsDocument": data.get("sendMediaAsDocument", False),
+            "sendAudioAsVoice": data.get("sendAudioAsVoice", False),
+            "sendVideoAsGif": data.get("sendVideoAsGif", False),
+            "sendMediaAsSticker": data.get("sendMediaAsSticker", False),
+            "sendMediaAsHd": data.get("sendMediaAsHd", False),
+            "isViewOnce": data.get("isViewOnce", False),
+            "linkPreview": data.get("linkPreview", False),
+        }
+
+        await self.hass.services.async_call(
+            "whatsapp",
+            "send_media",
+            payload,
+            blocking=True,
+        )
